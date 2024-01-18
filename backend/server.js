7baf94545325a99cff3a79fd716fe61a9769f050
Tom Scullion <tom@tomscullion.dev>
2024-01-18 10:49:33 +0000

    fix typo in server.js file

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "/frontemd/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   // any route that is not api will be redirected to inde.html
   app.get("*", (req, res) => {
